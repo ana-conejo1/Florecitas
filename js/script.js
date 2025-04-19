127550c1cb253e591e291d8cd46b75269aadfd3d
Diana <diana.alvarezdev@gmail.com>
2025-04-19 01:06:50 -0600

    arreglos en la navbar

--- js/script.js
+++ js/script.js
@@ -1,54 +1,51 @@
 /*Permite llamar a la barra de navegación en todas las páginas que se requiera*/
 fetch(`/html/navbar.html`)
-  .then(response => response.text()) // Se añadió return aquí
-  .then(data => {
+  .then((response) => response.text()) // Se añadió return aquí
+  .then((data) => {
     document.querySelector(`#navbar`).innerHTML = data;
-  })
-  .catch(error => {
-    console.log(`Error al cargar navbar:`, error);
-  });
 
-// Footer
-fetch(`/html/footer.html`)
-  .then(response => response.text()) // Se añadió return aquí
-  .then(data => {
-    document.querySelector(`#footer`).innerHTML = data;
-  })
-  .catch(error => {
-    console.log(`Error al cargar footer:`, error);
-  });
+    const loggedInUser = JSON.parse(localStorage.getItem("login_success"));
+    const userMenu = document.getElementById("userMenu");
 
-// verificar si se inicio sesion
-window.onload = function () {
-  const loggedInUser = JSON.parse(localStorage.getItem("login_success"));
-  const userMenu = document.getElementById('userMenu');
-
-  if (loggedInUser) {
-    userMenu.innerHTML = `
+    if (loggedInUser) {
+      userMenu.innerHTML = `
           <li><a class="dropdown-item" href="#">Mi cuenta</a></li>
           <li><hr class="dropdown-divider"></li>
           <li><button class="dropdown-item" id="signOutButton">Cerrar sesión</button></li>
       `;
 
-    // Cerrar sesión
-    document.getElementById("signOutButton").addEventListener("click", function () {
-      localStorage.removeItem("login_success");
-      alert("Has cerrado sesión.");
-      window.location.reload();
-    });
-  } else {
-    userMenu.innerHTML = `
+      // Cerrar sesión
+      document
+        .getElementById("signOutButton")
+        .addEventListener("click", function () {
+          localStorage.removeItem("login_success");
+          alert("Has cerrado sesión.");
+          window.location.reload();
+        });
+    } else {
+      userMenu.innerHTML = `
           <li><a class="dropdown-item" href="/html/login.html">Iniciar sesión</a></li>
           <li><a class="dropdown-item" href="/formulario-registro/form-registro-usuarios.html">Registrarse</a></li>
       `;
-  }
-};
+    }
+  })
+  .catch((error) => {
+    console.log(`Error al cargar navbar:`, error);
+  });
 
+// Footer
+fetch(`/html/footer.html`)
+  .then((response) => response.text()) // Se añadió return aquí
+  .then((data) => {
+    document.querySelector(`#footer`).innerHTML = data;
+  })
+  .catch((error) => {
+    console.log(`Error al cargar footer:`, error);
+  });
 
 function redirectTo(page) {
   window.location.href = page;
 }
-
 
 //Dinamismo a la parte de equipo
 var swiper = new Swiper(".mySwiper", {
@@ -62,44 +59,42 @@ var swiper = new Swiper(".mySwiper", {
     // cuando la pantalla sea >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 10
+      spaceBetween: 10,
     },
     // cuando la pantalla sea >= 640px
     640: {
       slidesPerView: 2,
-      spaceBetween: 20
+      spaceBetween: 20,
     },
     // cuando la pantalla sea >= 1024px
     1024: {
       slidesPerView: 3,
-      spaceBetween: 30
-    }
-  }
+      spaceBetween: 30,
+    },
+  },
 });
 
 //###### Efecto maquina de escribir ########
 // Seleccionar elementos del DOM
-var app = document.getElementById('app');
-var text = document.getElementById('text');
+var app = document.getElementById("app");
+var text = document.getElementById("text");
 
 if (app) {
-    var typewriter1 = new Typewriter(app, {
-        loop: true
-    });
-    typewriter1.typeString('¡Únete a nosotras en este viaje hacia un futuro más verde!')
-        .pauseFor(2500)
-        .deleteAll()
-        .start();
+  var typewriter1 = new Typewriter(app, {
+    loop: true,
+  });
+  typewriter1
+    .typeString("¡Únete a nosotras en este viaje hacia un futuro más verde!")
+    .pauseFor(2500)
+    .deleteAll()
+    .start();
 }
 
 if (text) {
-    var typewriter2 = new Typewriter(text, {
-        loop: true
-    });
-    typewriter2.typeString('Desarrolladoras')
-        .pauseFor(2500)
-        .deleteAll()
-        .start();
+  var typewriter2 = new Typewriter(text, {
+    loop: true,
+  });
+  typewriter2.typeString("Desarrolladoras").pauseFor(2500).deleteAll().start();
 }
 
 /*############### CARRUSEL DE IMÁGENES ###############*/
